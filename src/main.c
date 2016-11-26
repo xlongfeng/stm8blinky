2e26d0c748089be3e8dd55e8e0c18d94124922f6
xlongfeng <xlongfeng@126.com>
2016-11-26 17:53:23 +0800

    Implement the blinking code
    Signed-off-by: xlongfeng <xlongfeng@126.com>

--- src/main.c
+++ src/main.c
@@ -1,11 +1,36 @@
 #include <stdint.h>
 
+#include "stm8s.h"
+
+#ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line)
 {
+    /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
+    /* Infinite loop */
+    while (1) {
+    }
+}
+#endif
+
+void delay(uint16_t nCount)
+{
+    /* Decrement nCount value */
+    while (nCount != 0) {
+        nCount--;
+    }
 }
 
 int main()
 {
-	return 0;
+    /* Initialize I/Os in Output Mode */
+    GPIO_Init(GPIOB, (GPIO_Pin_TypeDef)GPIO_PIN_5, GPIO_MODE_OUT_OD_LOW_SLOW);
+
+    while (1) {
+        GPIO_WriteReverse(GPIOB, (GPIO_Pin_TypeDef)GPIO_PIN_5);
+        delay(0xFFFF);
+    }
+
+    return 0;
 }
